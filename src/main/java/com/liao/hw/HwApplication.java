c1af3e4bc3f3948fc0adee7cabd3b3164bc89d19
liaobaocai <760003259@qq.com>
2019-11-07 17:49:43 +0800

    项目初始化

---
+++ src/main/java/com/liao/hw/HwApplication.java
@@ -0,0 +1,14 @@
+package com.liao.hw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication  //springboot应用标注，主程序类
+public class HwApplication {
+
+	public static void main(String[] args) {
+		//启动应用
+		SpringApplication.run(HwApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/liao/hw/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.liao.hw;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HwApplication.class);
+	}
+
+}
